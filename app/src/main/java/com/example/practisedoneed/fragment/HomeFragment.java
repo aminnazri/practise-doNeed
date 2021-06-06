663662f3c513689fae913e1e84b29980e8656e0a
Abu Bakar <abbakker00@gmail.com>
2021-06-06 18:35:24 +0800

    new popup filter UI

--- app/src/main/java/com/example/practisedoneed/fragment/HomeFragment.java
+++ app/src/main/java/com/example/practisedoneed/fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.practisedoneed.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -8,10 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
@@ -38,10 +41,13 @@ import com.example.practisedoneed.adapter.donateAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private RecyclerView recyclerView;
     private donateAdapter postAdapter;
@@ -51,9 +57,18 @@ public class HomeFragment extends Fragment {
     private RecyclerView recyclerView_story;
     RecyclerView.LayoutManager linearLayoutManager;
     private Toolbar toolbar;
-
     private TextView doNeedTitle;
 
+    private TextView textState;
+    private boolean[] selectedState;
+    String[] stateArray = {"All","Johor","Kedah","Kelantan","Melaka","Negeri Sembilan","Pahang","Penang","Perak","Perlis"
+                        ,"Sabah","Sarawak","Selangor","Terengganu","Kuala Lumpur","Labuan","Putrajaya"};
+    final List<String> stateList = Arrays.asList(stateArray);
+
+    private TextView textCategory;
+    private boolean[] selectedCategory;
+    String[] categoryArray = {"All","Home Equipment","Furniture","Computer","Smartphone","Technology","Cloth","Sport"};
+    final List<String> categoryList = Arrays.asList(categoryArray);
 
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -64,6 +79,16 @@ public class HomeFragment extends Fragment {
         ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
         doNeedTitle = toolbar.findViewById(R.id.text_doNeed);
         doNeedTitle.setVisibility(View.VISIBLE);
+
+        textState = view.findViewById(R.id.state_filter);
+        selectedState = new boolean[stateArray.length];
+        textState.setOnClickListener(this);
+        selectedState[0] = true;
+
+        textCategory = view.findViewById(R.id.category_filter);
+        selectedCategory = new boolean[categoryArray.length];
+        textCategory.setOnClickListener(this);
+        selectedCategory[0] = true;
 
         return view;
     }
@@ -80,5 +105,131 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
 //        binding = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.state_filter){
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Select State")
+                    .setCancelable(false);
+            builder.setMultiChoiceItems(stateArray, selectedState, new DialogInterface.OnMultiChoiceClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+//                    if(isChecked){
+//                        if(!stateList.contains(which)){
+//                            stateList.add(which);
+//                            Collections.sort(stateList);
+//                        }
+//                    }else {
+//                        if(stateList.contains(which)){
+//                            stateList.remove(which);
+//                        }
+//                    }
+
+                    selectedState[which] = isChecked;
+                    String currentItem = stateList.get(which);
+                    Toast.makeText(getActivity(), currentItem+ " "+ isChecked, Toast.LENGTH_SHORT).show();
+
+                    for(int i=1; i<selectedState.length; i++){
+                        if(selectedState[i]){
+                            selectedState[0] = false;
+                            break;
+                        }
+                    }
+                }
+            });
+
+            builder.setPositiveButton("APPLY", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //get true item n search
+                    String SB = "";
+                    for (int i = 0; i<selectedState.length; i++){
+                        boolean checked = selectedState[i];
+                        if (checked) {
+                            SB = SB + stateList.get(i) + " ";
+                        }
+                    }
+                    Toast.makeText(getActivity(), SB, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setNeutralButton("CLEAR ALL", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+//                    for(int i=0; i<selectedState.length; i++){
+//                        selectedState[i]=false;
+//                        stateList.clear();
+//                    }
+                    Arrays.fill(selectedState, false);
+                    selectedState[0] = true;
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
+        else if(v.getId()==R.id.category_filter){
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Select Category")
+                    .setCancelable(false);
+            builder.setMultiChoiceItems(categoryArray, selectedCategory, new DialogInterface.OnMultiChoiceClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                    selectedCategory[which] = isChecked;
+                    String currentItem = categoryList.get(which);
+                    Toast.makeText(getActivity(), currentItem+ " "+ isChecked, Toast.LENGTH_SHORT).show();
+
+                    for(int i=1; i<selectedCategory.length; i++){
+                        if(selectedCategory[i]){
+                            selectedCategory[0] = false;
+                            break;
+                        }
+                    }
+                }
+            });
+
+            builder.setPositiveButton("APPLY", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //get true item n search
+                    String SB = "";
+                    for (int i = 0; i<selectedCategory.length; i++){
+                        boolean checked = selectedCategory[i];
+                        if (checked) {
+                            SB = SB + categoryList.get(i) + " ";
+                        }
+                    }
+                    Toast.makeText(getActivity(), SB, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            builder.setNeutralButton("CLEAR ALL", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Arrays.fill(selectedCategory, false);
+                    selectedCategory[0] = true;
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
     }
 }
